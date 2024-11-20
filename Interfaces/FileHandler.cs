3e17d471e3d18e714835345574aa428df1abc2dc
JuliailuL <julia.schneidewind@yahoo.com>
2024-11-20 11:26:20 +0100

    some more comments on Interfaces

--- Interfaces/FileHandler.cs
+++ Interfaces/FileHandler.cs
@@ -1,7 +1,10 @@
 using System;
 
 namespace Interfaces;
-
+/// <summary>
+/// Ein Interdace ist ein Vertrag. Wer ein Interface implementiert muss alle Member (Eigenschaften und Methoden) des Interfaces implementieren.
+/// Ein Interface ist eine Blaupause für eine Klasse / ein Struct, die beschreibt welche Methoden und Eigenschaften die Klasse / das Struct haben muss, aber nicht wie die Methoden / Eigenschaften sein müssen
+/// </summary>
 public interface FileHandler
 {
     public string FilePath { get; set; }
